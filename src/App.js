68444e4f3dd8a818beb20500983a0fe1a2cc719a
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-28 18:54:50 +0530

    completed styling post header, caption and image

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="instagram" />
         </div>
         <Post />
+        <Post />
+        <Post />
       </div>
     );
   }
