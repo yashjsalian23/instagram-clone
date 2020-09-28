b36a107fdbcd42aac8036beb915c751fe52356f1
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-28 17:40:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
