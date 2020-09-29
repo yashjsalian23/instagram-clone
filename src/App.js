067eab4ae2b8f152decdba4a5fa6d800fb9c955a
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-29 12:27:38 +0530

    sorted post according to time

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () =>   {
   }, [user, username])
 
   useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       setPosts(snapshot.docs.map(doc => ({id: doc.id, post: doc.data()})));
     })
   }, []);
